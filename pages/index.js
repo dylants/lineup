34c70c447ef529b279f29d16adf31a6ad29e33a5
Dylan Smith <dylantsmith@gmail.com>
2022-10-12 17:22:58 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Lineup</title>
+      </Head>
+
+      <main>
+        <h1>
+          Lineup
+        </h1>
+      </main>
+    </>
+  )
+}
