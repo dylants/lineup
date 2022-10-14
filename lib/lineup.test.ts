874d1f66e0d56bce5553ecbad7610a71067169d0
Dylan Smith <dylantsmith@gmail.com>
2022-10-14 17:24:23 -0500

    update lineup logic
    Switch the logic such that we allow for more than 5 players, and randomly choose the 'winners' that make the lineup from that group.

--- lib/lineup.test.ts
+++ lib/lineup.test.ts
@@ -18,11 +18,11 @@ describe('lineup', () => {
 
   describe('generateLineup with standard players', () => {
     const players = {
-      alpha: 'PG',
-      beta: 'SG',
-      gamma: 'SF',
-      delta: 'PF',
-      epsilon: 'C',
+      point: 'PG',
+      shoot: 'SG',
+      strong: 'SF',
+      power: 'PF',
+      center: 'C',
     };
     let lineup: { [key: string]: string };
     beforeAll(() => {
@@ -31,6 +31,40 @@ describe('lineup', () => {
 
     it('should generate a lineup with every player included', () => {
       expect(Object.keys(lineup).sort()).toEqual(Object.keys(players).sort());
+    });
+
+    it('should generate a lineup without duplicates', () => {
+      const lineupValues = _.values(lineup);
+      const uniqueLineupValues = _.uniq(lineupValues);
+      expect(lineupValues.length).toEqual(uniqueLineupValues.length);
+    });
+
+    it('should generate a lineup that contains each position', () => {
+      const lineupValues = _.values(lineup).sort();
+      expect(lineupValues).toEqual(['C', 'PF', 'PG', 'SF', 'SG']);
+    });
+  });
+
+  describe('generateLineup with large mix of players', () => {
+    const players = {
+      point1: 'PG',
+      point2: 'PG',
+      shoot1: 'SG',
+      power2: 'PF',
+      center2: 'C',
+      power3: 'PF',
+      strong1: 'SF',
+      power1: 'PF',
+      strong2: 'SF',
+      center1: 'C',
+    };
+    let lineup: { [key: string]: string };
+    beforeAll(() => {
+      lineup = numbers.generateLineup(players);
+    });
+
+    it('should generate a lineup of 5 players', () => {
+      expect(Object.keys(lineup).length).toEqual(5);
     });
 
     it('should generate a lineup without duplicates', () => {
--- lib/lineup.ts
+++ lib/lineup.ts
@@ -44,34 +44,41 @@ export function generateLineup(players: {}): { [key: string]: string } {
   const lineup: { [key: string]: string } = {};
   const positionsTaken = [];
 
-  _(players)
-    .keys()
-    .shuffle()
-    .forEach((playerName) => {
-      logger.info('finding position for player %s', playerName);
+  // randomize the players to select for the lineup
+  const playersAvailable = _(players).keys().shuffle().value();
 
-      // if we've got 4 positions found, just fill the last one
-      if (positionsTaken.length === 4) {
-        const remainingPosition = _.difference(_.range(5), positionsTaken)[0];
-        logger.info('remainingPosition %i', remainingPosition);
-        lineup[playerName] = getPositionNameFromIndex(remainingPosition);
-        return;
-      }
+  // fill a lineup of 5 players
+  while (positionsTaken.length < 5) {
+    const playerName = playersAvailable.shift();
+    logger.info('attempting to place player in lineup: %s', playerName);
 
-      const positionName = players[playerName];
-      const probabilities = getProbabilitiesForPosition(positionName);
+    // if this is the last player, assign to the available position
+    if (playersAvailable.length === 0) {
+      const remainingPosition = _.difference(_.range(5), positionsTaken)[0];
+      logger.info(
+        'only one player remaining, assigning to remainingPosition %i',
+        remainingPosition
+      );
+      lineup[playerName] = getPositionNameFromIndex(remainingPosition);
+      break;
+    }
 
-      let positionIndex: number;
-      do {
-        logger.info('looking for position...');
-        positionIndex = generateRandomPosition(probabilities);
-        logger.info('position index %i', positionIndex);
-      } while (positionsTaken.includes(positionIndex));
+    const positionName = players[playerName];
+    const probabilities = getProbabilitiesForPosition(positionName);
 
+    const positionIndex = generateRandomPosition(probabilities);
+    logger.info('position index %i', positionIndex);
+
+    if (positionsTaken.includes(positionIndex)) {
+      logger.info('already have a player at this position, moving on...');
+      playersAvailable.push(playerName);
+    } else {
+      // assign this player to the position
       positionsTaken.push(positionIndex);
       logger.info('positionsTaken %j', positionsTaken);
       lineup[playerName] = getPositionNameFromIndex(positionIndex);
-    });
+    }
+  }
 
   logger.info('lineup %j', lineup);
   return lineup;
