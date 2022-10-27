571780d107317d32f42e83ff3a81a82bb6e4bc2b
Dylan Smith <dylantsmith@gmail.com>
2022-10-26 20:52:49 -0500

    fix imports

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
-import Game from '../../lib/Game';
+import Game from '../../lib/game/Game';
 import { generateRandomPlayers } from '../../lib/utils';
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
