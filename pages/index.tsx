8141a0654a8a2d2ead4777c777a807389d91220d
Dylan Smith <dylantsmith@gmail.com>
2022-10-27 19:59:38 -0500

    Connect data to the page
    Add lineups to the page via an API call using SWR. Include some basic styling for now.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,43 @@
 import Head from 'next/head';
+import useSWR from 'swr';
+import fetcher from '../lib/fetcher';
+import Assignment from '../lib/game/Assignment';
+import Game from '../lib/game/Game';
+import Lineup from '../lib/game/Lineup';
 
 export default function Home() {
+  const { data, mutate, error } = useSWR<Game, Error>('/api/game', fetcher, {
+    revalidateOnFocus: false,
+  });
+
+  if (error) return <div>error</div>;
+  if (!data) return <div>loading...</div>;
+
+  const lineups = data.lineups.map((lineup: Lineup) => {
+    const lineupRows = lineup.assignments.map((assignment: Assignment) => {
+      return (
+        <tr key={assignment.player.name}>
+          <td>{assignment.position.name}</td>
+          <td>{assignment.player.name}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table key={lineup.frame}>
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Player</th>
+          </tr>
+        </thead>
+        <tbody>{lineupRows}</tbody>
+      </table>
+    );
+  });
+
+  const lineupsToDisplay = <div className="lineups">{lineups}</div>;
+
   return (
     <>
       <Head>
@@ -9,6 +46,10 @@ export default function Home() {
 
       <main>
         <h1>Lineup</h1>
+        {lineupsToDisplay}
+        <div className="buttons">
+          <button onClick={() => mutate()}>Regenerate</button>
+        </div>
       </main>
     </>
   );
